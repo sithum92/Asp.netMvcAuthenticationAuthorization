2c4556e840eb6738c4e4ce3d4a657da99682d21a
Sithum Witharana <witharana92@gmail.com>
2023-01-08 15:02:31 +0530

    Add project files.

---
+++ Asp.netMvcAuthenticationAuthorization/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Asp.netMvcAuthenticationAuthorization.MvcApplication" Language="C#" %>
---
+++ Asp.netMvcAuthenticationAuthorization/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Asp.netMvcAuthenticationAuthorization.Startup))]
+namespace Asp.netMvcAuthenticationAuthorization
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
